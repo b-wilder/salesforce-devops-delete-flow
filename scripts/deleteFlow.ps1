4d2e594e203e15d43d3a0502e2eb059ff8536ac7
Brock Elgart <brock.elgart@steampunk.com>
2022-02-24 18:12:45 -0500

    rename empty flowdefinition

--- scripts/deleteFlow.ps1
+++ scripts/deleteFlow.ps1
@@ -1,6 +1,8 @@
-#powershell -ExecutionPolicy ByPass -File ./scripts/deleteFlow.ps1 'AppSuite Wizard'
+#powershell -ExecutionPolicy ByPass -File ./scripts/deleteFlow.ps1 'AppSuite Wizard' AppSuite_Wizard
 $apexScript = (Get-Content -path scripts\deleteFlowInterviewsTemplate.apex -Raw) -replace '__FLOW_NAME__', $args[0]
 New-Item scripts\deleteFlowInterviews.apex
 Set-Content scripts\deleteFlowInterviews.apex "$apexScript"
 sfdx force:apex:execute -f scripts\deleteFlowInterviews.apex
 Remove-Item scripts\deleteFlowInterviews.apex
+$FlowName = $args[1] + '.flowDefinition-meta.xml'
+Rename-Item -Path 'cd\deactivateFlow\Flow_Name.flowDefinition-meta.xml' -NewName "$FlowName"
